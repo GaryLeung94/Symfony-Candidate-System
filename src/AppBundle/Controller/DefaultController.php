9f5b7a93b7a378c0e037f16de32fe149ad5911fc
Gary Leung <garyliangljl@outlook.com>
2018-05-02 02:28:41 +0800

    successfully test the form submissions to MongoDB

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use CoolBundle\Document\Candidates;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -18,4 +23,43 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request)
+    {
+        $candidates = new Candidates();
+
+        $form = $this->createFormBuilder($candidates)
+            ->add('name', TextType::class)
+            ->add('sex', ChoiceType::class, array(
+                'choices' => array(
+                    '男' => '男',
+                    '女' => '女',
+                ),
+            ))
+            ->add('birthday', DateType::class, array(
+                'widget' => 'single_text',
+            ))
+            ->add('save', SubmitType::class, array('label' => '提交'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $candidates = $form->getData();
+
+            $dm = $this->get('doctrine_mongodb')->getManager();
+            $dm->persist($candidates);
+            $dm->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/new.html.twig', array('form' => $form->createView(),
+            ));
+    }
+
 }
